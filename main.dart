0a049965b3744dbbdd1dd79e18c421049e6b31ab
Ollohberdi <85603736+shokirovollohberdi@users.noreply.github.com>
2022-07-07 11:52:01 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,50 @@
+void main() {
+  /*for top to bottom
+	for(int i=20;i>=10;i--){
+    print(i);
+  }
+  */
+
+
+  /* for bottom to top
+  for(int i=1;i<=10;i++){
+    print(i);
+  }
+  */
+
+  /* 
+read the string by for
+  var word = "hello";
+  for(int i = 0;i<=word.length-1;i++){
+    print(word[i]);
+  }
+  */
+
+  /* reverse read the string by for
+
+  var word = "hello";
+for(int i=word.length-1;i>=0;i--){
+    print(word[i]);
+  }
+  */
+
+/* break oparation with for
+   for(int i=1;i<=30;i++){
+    if(i==17){
+      break;
+    }
+     print(i);
+  }
+*/
+
+  /*continue oparation with for
+
+  for(int i=1;i<=10;i++){
+    if(i%2==0){
+      continue;
+    }
+    print(i);
+  }
+ */
+  
+}
